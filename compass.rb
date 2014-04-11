8105cfe80849cdcdcafb2be138720e87b8e6f685
Martin Dilling-Hansen <martindilling@gmail.com>
2014-04-11 02:23:08 +0200

    Startup with Gulp starter template from https://github.com/greypants/gulp-starter

---
+++ compass.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :sass
+http_path = 'public/'
+css_dir = 'public/build'
+sass_dir = 'src/sass'
+images_dir = 'public/build/images'
+fonts_dir = 'public/build/fonts'
+relative_assets = true
+line_comments = true
