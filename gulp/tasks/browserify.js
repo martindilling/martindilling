7f2c727f8be9ac3b8b1322cc2cd6f84227cdfafd
Martin Dilling-Hansen <martindilling@gmail.com>
2014-04-14 05:19:49 +0200

    Minify js

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -3,18 +3,21 @@ var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 var notify = require("gulp-notify");
 var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
 
 module.exports = function() {
-	return browserify({
-			entries: ['./src/javascript/app.coffee'],
-			extensions: ['.coffee', '.hbs']
-		})
-		.require('backbone/node_modules/underscore', { expose: 'underscore' })
-		.bundle({debug: true})
-		.on('error', notify.onError({
-			message: "<%= error.message %>",
-			title: "JavaScript Error"
-		}))
-		.pipe(source('app.js'))
-		.pipe(gulp.dest('./public/build/'));
+    return browserify({
+            entries: ['./src/javascript/app.coffee'],
+            extensions: ['.coffee', '.hbs']
+        })
+        .require('backbone/node_modules/underscore', { expose: 'underscore' })
+        .bundle({debug: true})
+        .on('error', notify.onError({
+            message: "<%= error.message %>",
+            title: "JavaScript Error"
+        }))
+        .pipe(source('app.js'))
+        .pipe(streamify(uglify()))
+        .pipe(gulp.dest('./public/build/'));
 };
