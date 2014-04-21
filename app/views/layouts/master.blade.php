2202e2cb720d0b42839f12fa5b7a773b80d409a3
Martin Dilling-Hansen <martindilling@gmail.com>
2014-04-22 01:58:02 +0200

    Trying to get disqus to work

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -88,17 +88,17 @@
 {{ HTML::script('build/app.js') }}
 @yield('scripts')
 
-{{--<script type="text/javascript">
-    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-    var disqus_shortname = '@yield('disqus_shortname')';
-
-    /* * * DON'T EDIT BELOW THIS LINE * * */
-    (function () {
-        var s = document.createElement('script'); s.async = true;
-        s.type = 'text/javascript';
-        s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
-        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-    }());
-</script>--}}
+<script type="text/javascript">
+//    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+//    var disqus_shortname = '@yield('disqus_shortname')';
+//
+//    /* * * DON'T EDIT BELOW THIS LINE * * */
+//    (function () {
+//        var s = document.createElement('script'); s.async = true;
+//        s.type = 'text/javascript';
+//        s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+//        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+//    }());
+</script>
 </body>
 </html>
