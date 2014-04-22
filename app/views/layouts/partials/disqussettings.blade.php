5157fa7d9209104669c13bebe30b9eccc27025c0
Martin Dilling-Hansen <martindilling@gmail.com>
2014-04-22 02:02:45 +0200

    comment out disqus_category_id

--- app/views/layouts/partials/disqussettings.blade.php
+++ app/views/layouts/partials/disqussettings.blade.php
@@ -3,5 +3,5 @@
     var disqus_identifier  = '@yield('disqus_identifier')';
     var disqus_title       = '@yield('disqus_title')';
     var disqus_url         = '@yield('disqus_url')';
-    var disqus_category_id = '@yield('disqus_category_id')';
+//    var disqus_category_id = '@yield('disqus_category_id')';
 </script>
