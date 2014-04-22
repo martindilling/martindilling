10833a253452329e7bef3163a1b80464f0427503
Martin Dilling-Hansen <martindilling@gmail.com>
2014-04-22 02:05:39 +0200

    Fix category ids for disqus

--- app/config/disqus.php
+++ app/config/disqus.php
@@ -5,8 +5,8 @@ return array(
     'shortname' => 'martindilling',
     'category'  => [
         'general'  => '3016501',
-        'creation' => '3016678',
-        'post'     => '3016677',
+        'creation' => '3016679',
+        'post'     => '3016680',
     ],
 
 );
